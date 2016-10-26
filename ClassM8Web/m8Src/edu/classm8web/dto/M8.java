04e16728b068cc215bd2940b38271d688e1ef27f
Lukas Kirchbaumer <l.kirchbaumer1998@gmail.com>
2016-10-26 22:34:57 +0200

    Apply JPA integration

--- ClassM8Web/m8Src/edu/classm8web/dto/M8.java
+++ ClassM8Web/m8Src/edu/classm8web/dto/M8.java
@@ -1,9 +1,23 @@
 package edu.classm8web.dto;
 
+import java.io.Serializable;
 import java.util.Arrays;
 
-public class M8 {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
+public class M8 implements Serializable{
+
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7576437934172296816L;
+
+	@Id @GeneratedValue(strategy=GenerationType.AUTO)
 	private long id;
 	
 	private String firstname;
