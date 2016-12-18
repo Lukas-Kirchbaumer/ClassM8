0f58b85e0f87f3cbfeb9666bb55064c4b88574d2
Martin Laubreiter <Martin Laubreiter>
2016-12-18 17:12:57 +0100

    implemented WebService Layer

--- Android/backend/src/main/java/com/example/backend/Interfaces/DataReader.java
+++ Android/backend/src/main/java/com/example/backend/Interfaces/DataReader.java
@@ -1,15 +1,17 @@
 package com.example.backend.Interfaces;
 
 import com.example.backend.Dto.M8;
+import com.example.backend.Dto.MappedSchoolclass;
 import com.example.backend.Dto.Schoolclass;
 import com.example.backend.Executer;
+import com.example.backend.Results.LoginResult;
+import com.example.backend.Results.M8Result;
+import com.example.backend.Results.SchoolclassResult;
 import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
 
-import java.io.BufferedInputStream;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.List;
 
 /**
  * Created by laubi on 12/2/2016.
@@ -17,46 +19,201 @@ import java.util.List;
 
 public class DataReader implements InterfaceBetweenFrontAndBackendInterface {
 
-    Executer executer;
+    private Executer executer = new Executer();;
+    private Gson gson = new Gson();
+    private JsonParser parser = new JsonParser();
 
+    private DataReader instance = null;
+
+    /**
+     * DataReader is a Singleton. getInstance() returns the instance (duh)
+     * @return the Instance of DataReader
+     */
+    public DataReader getInstance(){
+        if(instance == null){
+            instance = new DataReader();
+        }
+        return instance;
+    }
+
+    /**
+     * Selfexplaining
+     * Returns null if Authentification failed
+     * @param email The E-Mail to verify
+     * @param password The Password to verify
+     * @return The Instance of the logged in User
+     */
     @Override
     public M8 login(String email, String password) {
-        M8 user = null;
+
+        M8 user = new M8();
+        user.setEmail(email);
+        user.setPassword(password);
+
         try {
-            URL serverURL = new URL("http://androidexample.com/media/webservice/JsonReturn.php");
-            HttpURLConnection urlConnection = (HttpURLConnection) serverURL.openConnection();
-            urlConnection.setRequestMethod("POST");
-            urlConnection.setRequestProperty("Accept", "application/json");
+            URL serverURL = new URL("http://localhost:8080/ClassM8Web/services/login");
 
-            executer = new Executer();
+            executer.setMethod("POST");
+            executer.setData(gson.toJson(user, M8.class));
+            executer.execute(serverURL);
 
-            executer.execute(urlConnection);
             String strFromWebService = executer.get();
 
             System.out.println("returned string: " + strFromWebService);
 
-            Gson gson = new Gson();
+            JsonElement o = parser.parse(strFromWebService);
+            LoginResult r = gson.fromJson(o, LoginResult.class);
+
+
+            serverURL = new URL("http://localhost:8080/ClassM8Web/services/user/"+r.getId());
+
+
+            executer.setMethod("GET");
+            executer.execute(serverURL);
+
+            strFromWebService = executer.get();
+
+            System.out.println("returned string: " + strFromWebService);
+
             user = gson.fromJson(strFromWebService, M8.class);
 
-        }catch (Exception e){
+            o = parser.parse(strFromWebService);
+            M8Result m8r = gson.fromJson(o, M8Result.class);
+            user = m8r.getContent().get(0);
 
+
+        }catch (Exception e){
+            user = null;
+            e.printStackTrace();
         }
         return user;
     }
 
-    @Override
-    public Schoolclass getSchoolclassByUser(M8 User) {
-      //  String serverURL = "http://androidexample.com/media/webservice/JsonReturn.php";
-        //  new Executer().execute(serverURL);
+    /**
+     * Method to get the Schoolclass of a specific User
+     * @param user The logged in User
+     * @return the Schoolclass of the User
+     */
+        @Override
+        public Schoolclass getSchoolclassByUser(M8 user) {
+            Schoolclass schoolclass = null;
+            try {
+                URL serverURL = new URL("http://localhost:8080/ClassM8Web/services/schoolclass/"+ user.getId());
 
-        return null;
+                executer.setMethod("GET");
+                executer.execute(serverURL);
+
+                String strFromWebService = executer.get();
+
+                System.out.println("returned string: " + strFromWebService);
+
+                JsonElement o = parser.parse(strFromWebService);
+                SchoolclassResult r = gson.fromJson(o, SchoolclassResult.class);
+
+                MappedSchoolclass mappedSchoolclass = r.getSchoolclasses().get(0);
+
+                schoolclass = mappedSchoolclass.toSchoolClass();
+
+
+            }catch (Exception e){
+                schoolclass = null;
+                e.printStackTrace();
+            }
+        return schoolclass;
     }
 
     @Override
     public Schoolclass createNewClass(String name, String school, String room) {
-        //  String serverURL = "http://androidexample.com/media/webservice/JsonReturn.php";
-        //  new Executer().execute(serverURL);
-        return null;
+        Schoolclass s = new Schoolclass();
+        s.setName(name);
+        s.setSchool(school);
+        s.setRoom(room);
+
+
+        try {
+            URL serverURL = new URL("http://localhost:8080/ClassM8Web/services/schoolclass");
+
+            executer.setMethod("POST");
+            executer.setData(gson.toJson(s, Schoolclass.class));
+            executer.execute(serverURL);
+
+            String strFromWebService = executer.get();
+
+            System.out.println("returned string: " + strFromWebService);
+
+            JsonElement o = parser.parse(strFromWebService);
+            LoginResult r = gson.fromJson(o, LoginResult.class);
+        }catch (Exception e){
+            s= null;
+            e.printStackTrace();
+        }
+        return s;
+    }
+
+
+    @Override
+    public Schoolclass updateClass(String name, String school, String room, Schoolclass oldSchoolClass) {
+        Schoolclass newSchoolclass = oldSchoolClass;
+        newSchoolclass.setRoom(room);
+        newSchoolclass.setSchool(school);
+        newSchoolclass.setName(name);
+
+        try {
+            URL serverURL = new URL("http://localhost:8080/ClassM8Web/services/schoolclass/" + oldSchoolClass.getId());
+
+            executer.setMethod("PUT");
+            executer.setData(gson.toJson(newSchoolclass, Schoolclass.class));
+            executer.execute(serverURL);
+
+            String strFromWebService = executer.get();
+
+            System.out.println("returned string: " + strFromWebService);
+
+            JsonElement o = parser.parse(strFromWebService);
+
+        } catch (Exception e) {
+            newSchoolclass = null;
+            e.printStackTrace();
+        }
+        return newSchoolclass;
+    }
+
+    @Override
+    public void updateClass(Schoolclass schoolClass) {
+        try {
+            URL serverURL = new URL("http://localhost:8080/ClassM8Web/services/schoolclass/"+schoolClass.getId());
+
+            executer.setMethod("PUT");
+            executer.setData(gson.toJson(schoolClass, Schoolclass.class));
+            executer.execute(serverURL);
+
+            String strFromWebService = executer.get();
+
+            System.out.println("returned string: " + strFromWebService);
+
+            JsonElement o = parser.parse(strFromWebService);;
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void deleteClass(Schoolclass schoolClass) {
+        try {
+            URL serverURL = new URL("http://localhost:8080/ClassM8Web/services/schoolclass/"+schoolClass.getId());
+
+            executer.setMethod("DELETE");
+            executer.setData(gson.toJson(schoolClass, Schoolclass.class));
+            executer.execute(serverURL);
+
+            String strFromWebService = executer.get();
+
+            System.out.println("returned string: " + strFromWebService);
+
+            JsonElement o = parser.parse(strFromWebService);;
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -68,43 +225,81 @@ public class DataReader implements InterfaceBetweenFrontAndBackendInterface {
     }
 
     @Override
-    public void deleteClass(Class schoolClass) {
-        //   String serverURL = "http://androidexample.com/media/webservice/JsonReturn.php";
-        //   new Executer().execute(serverURL);
-    }
-
-    @Override
-    public Class updateClass(String name, String school, String room, Class OldSchoolClass) {
-        //   String serverURL = "http://androidexample.com/media/webservice/JsonReturn.php";
-        //   new Executer().execute(serverURL);
-
-        return null;
-    }
-
-    @Override
-    public void updateClass(Class schoolClass) {
-        //   String serverURL = "http://androidexample.com/media/webservice/JsonReturn.php";
-        //new Executer().execute(serverURL);
-
-    }
-
-    @Override
     public void deleteUser(M8 user) {
-        //  String serverURL = "http://androidexample.com/media/webservice/JsonReturn.php";
-        //  new Executer().execute(serverURL);
+        try {
+            URL serverURL = new URL("http://localhost:8080/ClassM8Web/services/user/"+user.getId());
 
+            executer.setMethod("DELETE");
+            executer.setData(gson.toJson(user, Schoolclass.class));
+            executer.execute(serverURL);
+
+            String strFromWebService = executer.get();
+
+            System.out.println("returned string: " + strFromWebService);
+
+            JsonElement o = parser.parse(strFromWebService);;
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void updateUser(M8 user) {
-        //   String serverURL = "http://androidexample.com/media/webservice/JsonReturn.php";
-        //   new Executer().execute(serverURL);
+        try {
+            URL serverURL = new URL("http://localhost:8080/ClassM8Web/services/user/" + user.getId());
+
+            executer.setMethod("PUT");
+            executer.setData(gson.toJson(user, M8.class));
+            executer.execute(serverURL);
+
+            String strFromWebService = executer.get();
+
+            System.out.println("returned string: " + strFromWebService);
+
+            JsonElement o = parser.parse(strFromWebService);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
+    /**
+     * Replaces the Old User with a new one
+     * @param firstname firstname of the newly generated
+     * @param lastname
+     * @param eMail
+     * @param password
+     * @param OldUser
+     * @return A new generated User
+     */
     @Override
-    public void updateUser(String firstname, String lastname, String eMail, String password, M8 OldUser) {
-        //   String serverURL = "http://androidexample.com/media/webservice/JsonReturn.php";
-        //   new Executer().execute(serverURL);
+    public M8 updateUser(String firstname, String lastname, String eMail, String password, M8 OldUser) {
+        M8 newUser = new M8();
+        newUser.setFirstname(firstname);
+        newUser.setLastname(lastname);
+        newUser.setEmail(eMail);
+        newUser.setPassword(password);
+        newUser.setId(OldUser.getId());
+        newUser.setVotes(OldUser.getVotes());
+
+        try {
+            URL serverURL = new URL("http://localhost:8080/ClassM8Web/services/user/" + OldUser.getId());
+
+            executer.setMethod("PUT");
+            executer.setData(gson.toJson(newUser, M8.class));
+            executer.execute(serverURL);
+
+            String strFromWebService = executer.get();
+
+            System.out.println("returned string: " + strFromWebService);
+
+            JsonElement o = parser.parse(strFromWebService);
+
+        } catch (Exception e) {
+            newUser = null;
+            e.printStackTrace();
+        }
+        return newUser;
     }
 
 
--- Android/backend/src/main/java/com/example/backend/Interfaces/InterfaceBetweenFrontAndBackendInterface.java
+++ Android/backend/src/main/java/com/example/backend/Interfaces/InterfaceBetweenFrontAndBackendInterface.java
@@ -12,10 +12,10 @@ interface InterfaceBetweenFrontAndBackendInterface {
     public Schoolclass getSchoolclassByUser(M8 user);
     public Schoolclass createNewClass (String name, String school, String room);
     public M8 createNewUser (String firstname, String lastname, String eMail, String password, String passwordConfirmation);
-    public void deleteClass(Class schoolClass);
-    public Class updateClass (String name, String school, String room, Class OldSchoolClass);
-    public void updateClass (Class schoolClass);
+    public void deleteClass(Schoolclass schoolClass);
+    public Schoolclass updateClass (String name, String school, String room, Schoolclass OldSchoolClass);
+    public void updateClass (Schoolclass schoolClass);
     public void deleteUser (M8 user);
     public void updateUser(M8 user);
-    public void updateUser(String firstname, String lastname, String eMail, String password, M8 OldUser);
+    public M8 updateUser(String firstname, String lastname, String eMail, String password, M8 OldUser);
 }
