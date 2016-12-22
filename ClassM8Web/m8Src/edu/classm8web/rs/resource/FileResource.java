511dcea9404ac111a8468a4ccd078ae19785850c
Lukas Kirchbaumer <l.kirchbaumer1998@gmail.com>
2016-12-22 21:26:09 +0100

    Download
    Signed-off-by: Lukas Kirchbaumer <l.kirchbaumer1998@gmail.com>

--- ClassM8Web/m8Src/edu/classm8web/rs/resource/FileResource.java
+++ ClassM8Web/m8Src/edu/classm8web/rs/resource/FileResource.java
@@ -1,5 +1,6 @@
 package edu.classm8web.rs.resource;
 
+import java.io.FileInputStream;
 import java.io.InputStream;
 import java.util.Date;
 
@@ -15,6 +16,9 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.ResponseBuilder;
 import javax.ws.rs.core.Response.Status;
 
+import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
+import org.glassfish.jersey.media.multipart.FormDataParam;
+
 import edu.classm8web.database.dao.FileService;
 import edu.classm8web.database.dao.SchoolclassService;
 import edu.classm8web.database.dto.File;
@@ -22,68 +26,66 @@ import edu.classm8web.database.dto.Schoolclass;
 import edu.classm8web.rs.result.Result;
 
 @Path("file")
-public class FileResource extends AbstractResource{
+public class FileResource extends AbstractResource {
 
+	
+	
 	@GET
 	@Path("content/{fileid}")
 	@Produces(value = { MediaType.APPLICATION_OCTET_STREAM })
-	public Response streamFileById(@PathParam("fileid") String fileid){
-		
+	public Response streamFileById(@PathParam("fileid") String fileid) {
+
 		Result r = new Result();
-		
+
 		ResponseBuilder builder = null;
 
 		try {
 			File file = FileService.getInstance().findById(Long.valueOf(fileid));
-			
-			if(file != null){
-				InputStream is = null; //stream file content
-				
-				builder = Response.status(Status.ACCEPTED).entity(is);
-				builder.type(file.getContentType());
-				builder.header("Content-Disposition", "attachment; filename=\"" + file.getFileName() + "\"");
-				
-			} else {
-				throw new Exception("File doesn't exist");
-			}
 
+			java.io.File f = new java.io.File(
+					"D:\\servers\\glassfish4\\glassfish\\domains\\cm8\\generated\\data\\" + file.getFileName());
+
+			InputStream is = new FileInputStream(f);
+
+			builder = Response.status(Status.ACCEPTED).entity(is);
+			builder.type(file.getContentType());
+			builder.header("Content-Disposition", "attachment; filename=\"" + file.getFileName() + "\"");
 
 		} catch (Exception e) {
 			handelAndThrowError(e, r);
 		}
-		
+
 		return builder.build();
 	}
-	
-	/*@POST
-	@Path("content/{fileid}")
-	@Consumes(MediaType.MULTIPART_FORM_DATA)
-	public Response uploadFile(@FormDataParam("file") InputStream uploadedInputStream,
-            @FormDataParam("file") FormDataContentDisposition fileDetail, @PathParam("fileid") String fileid){
-		return null;
-	}*/
-	
+
+//	@POST
+//	@Path("content/{fileid}")
+//	@Consumes(MediaType.MULTIPART_FORM_DATA)
+//	public Response uploadFile(@FormDataParam("file") InputStream uploadedInputStream,
+//			@FormDataParam("file") FormDataContentDisposition fileDetail, @PathParam("fileid") String fileid) {
+//		return null;
+//	}
+
 	@POST
 	@Produces(value = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
 	@Consumes("application/json")
-	public Response createFileMetaDataInGroup(@QueryParam ("schoolclassid") String schoolclassid, final File input){
-		
+	public Response createFileMetaDataInGroup(@QueryParam("schoolclassid") String schoolclassid, final File input) {
+
 		Result r = new Result();
-		
+
 		try {
 			Schoolclass s = SchoolclassService.getInstance().findById(Long.valueOf(schoolclassid));
-			if(s != null){
+			if (s != null) {
 				Date now = new Date();
 				input.setUploadDate(new java.sql.Date(now.getTime()));
 				FileService.getInstance().persist(input);
-				
-				
+
 				s.getFiles().add(input);
 				input.setReferencedSchoolclass(s);
-				
+
 				SchoolclassService.getInstance().update(s);
 				FileService.getInstance().update(input);
-				
+
 				r.setSuccess(true);
 
 			} else {
@@ -92,8 +94,7 @@ public class FileResource extends AbstractResource{
 		} catch (Exception e) {
 			handelAndThrowError(e, r);
 		}
-		
-		
+
 		return Response.status(Status.ACCEPTED).entity(r).build();
 	}
 }
