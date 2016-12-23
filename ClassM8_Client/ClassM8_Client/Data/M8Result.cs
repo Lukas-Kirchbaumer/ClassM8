194bdd15fd7a804eb8365f35f0aeef4530fd3fea
Lukas Kramer <lukaskramer@gmx.at>
2016-12-23 21:13:18 +0100

    added untracked files

---
+++ ClassM8_Client/ClassM8_Client/Data/M8Result.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassM8_Client.Data
+{
+    [DataContract]
+    class M8Result : Result
+    {
+        [DataMember]
+        private List<M8> content { get; set; }
+
+        public M8Result() { }
+
+        public List<M8> getM8s()
+        {
+            return this.content;
+        }
+
+    }
+}
