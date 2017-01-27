6dfd9c78e9c589d9914f6c661a2f9bcbaa3b6cc5
Martin Laubreiter <Martin Laubreiter>
2017-01-27 09:19:29 +0100

    added chat functions to datareader

--- Android/backend/src/main/java/com/example/backend/FileExecuter.java
+++ Android/backend/src/main/java/com/example/backend/FileExecuter.java
@@ -6,6 +6,7 @@ import android.os.AsyncTask;
 import com.example.backend.Dto.File;
 import com.example.backend.Interfaces.DataReader;
 
+import java.io.ByteArrayOutputStream;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.InputStream;
@@ -58,76 +59,63 @@ public class FileExecuter  extends AsyncTask<File, String,String>{
         }
         else{
             /*
-            System.out.println("Uploading "+ file+ " to "+ url);
-            String boundary = "---------------------------" + System.currentTimeMillis();
-            byte[] boundarybytes = ("\r\n--" + boundary + "\r\n").getBytes();
+            try {
+                URL serverURL = new URL("http://" + DataReader.IP + ":8080/ClassM8Web/services/file/content/" + file.getId());
 
+                System.out.println("Uploading " + file + " to " + serverURL.getPath());
+                String boundary = "---------------------------" + System.currentTimeMillis();
+                byte[] boundarybytes = ("\r\n--" + boundary + "\r\n").getBytes();
 
-            URL serverURL = new URL("http://" + DataReader.IP + ":8080/ClassM8Web/services/file/content/" + file.getId());
+                HttpURLConnection con = (HttpURLConnection) serverURL.openConnection();
 
-            HttpURLConnection con = (HttpURLConnection) serverURL.openConnection();
+                con.setConnectTimeout(1000);
 
-            con.setConnectTimeout(1000);
+                con.setDoOutput(true);    // indicates POST method
+                con.setDoInput(true);
+                con.setRequestProperty("Content-Type", "multipart/form-data; boundary=" + boundary);
 
-            con.setDoOutput(true);    // indicates POST method
-            con.setDoInput(true);
-            con.setRequestProperty("Content-Type", "multipart/form-data; boundary=" + boundary);
+                con.setRequestMethod("POST");
+                con.setRequestProperty("Connection", "Keep-Alive");
+                con.setRequestProperty("Keep-Alive", "header");
 
-            con.setRequestMethod("POST");
-            con.setRequestProperty("Connection", "Keep-Alive");
-            con.setRequestProperty("Keep-Alive", "header");
+                OutputStream outputStream = con.getOutputStream();
 
-            OutputStream outputStream = con.getOutputStream();
+                outputStream.write(boundarybytes, 0, boundarybytes.length);
 
-            outputStream.write(boundarybytes, 0, boundarybytes.length);
+                outputStream.write(boundarybytes, 0, boundarybytes.length);
 
-            outputStream.write(boundarybytes, 0, boundarybytes.length);
+                String header = "Content-Disposition: form-data; name=\"" + paramName + "+\"; filename=\"" + file + "\"\r\nContent-Type: " + contentType + "\r\n\r\n";
 
-            String header = "Content-Disposition: form-data; name=\""+paramName+"+\"; filename=\""+file+"\"\r\nContent-Type: "+contentType+"\r\n\r\n";
+                byte[] headerbytes = header.getBytes();
 
-            byte[] headerbytes = header.getBytes();
+                outputStream.write(headerbytes, 0, headerbytes.length);
 
-            outputStream.write(headerbytes, 0, headerbytes.length);
+                FileInputStream fileInputStream = new FileInputStream(file);
 
-            FileInputStream fileInputStream = new FileInputStream(file);
-
-            byte[] buffer = new byte[4096];
-            int bytesRead = 0;
-            while ((bytesRead = fileInputStream.read(buffer, 0, buffer.length)) != 0)
-            {
-                outputStream.write(buffer, 0, bytesRead);
-            }
-            fileInputStream.close();
-
-            byte[] trailer = ("\r\n--" + boundary + "--\r\n").getBytes();
-            outputStream.write(trailer, 0, trailer.length);
-            outputStream.close();
-
-            InputStream inputStream = con.getInputStream();
-            StringReader r = new StringReader()
-
-
-            WebResponse wresp = null;
-            try
-            {
-                wresp = wr.GetResponse();
-                Stream stream2 = wresp.GetResponseStream();
-                StreamReader reader2 = new StreamReader(stream2);
-                Console.WriteLine(string.Format("File uploaded, server response is: {0}", reader2.ReadToEnd()));
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Error uploading file", ex);
-                if (wresp != null)
-                {
-                    wresp.Close();
-                    wresp = null;
+                byte[] buffer = new byte[4096];
+                int bytesRead = 0;
+                while ((bytesRead = fileInputStream.read(buffer, 0, buffer.length)) != 0) {
+                    outputStream.write(buffer, 0, bytesRead);
                 }
-            }
-            finally
-            {
-                wr = null;
-            }
+                fileInputStream.close();
+
+                byte[] trailer = ("\r\n--" + boundary + "--\r\n").getBytes();
+                outputStream.write(trailer, 0, trailer.length);
+                outputStream.close();
+
+                InputStream inputStream = con.getInputStream();
+
+
+                ByteArrayOutputStream result = new ByteArrayOutputStream();
+                byte[] buffer2 = new byte[1024];
+                int length;
+                while ((length = inputStream.read(buffer2)) != -1) {
+                    result.write(buffer2, 0, length);
+                }
+                System.out.println(result.toString("UTF-8"));
+            }catch (Exception e){
+                e.printStackTrace();
+
         }*/
         }
         return "";
