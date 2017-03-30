afd369978fe826327213d5a920b17743b1ce216f
Max Haider <max.haider@outlook.com>
2017-03-30 20:58:58 +0200

    push

---
+++ ClassM8_Client/ClassM8_Client/Controls/NewClassControl.xaml.cs
@@ -0,0 +1,52 @@
+﻿using ClassM8_Client.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ClassM8_Client.Controls
+{
+    /// <summary>
+    /// Interaction logic for NewClassControl.xaml
+    /// </summary>
+    public partial class NewClassControl : UserControl
+    {
+        public NewClassControl()
+        {
+            InitializeComponent();
+        }
+
+        private void btnNewAccCreate_Click(object sender, RoutedEventArgs e)
+        {
+            txtError.Text = "";
+            createNewSchoolclass();
+            ControllerNavigator.NavigateTo(ControllerHolder.LoginControl);
+
+        }
+
+        private void btnNewAccCancle_Click(object sender, RoutedEventArgs e)
+        {
+            ControllerNavigator.NavigateTo(new NoFriendsControl());
+        }
+
+        public void createNewSchoolclass()
+        {
+            Schoolclass sc = new Schoolclass();
+            sc.setName(txtName.Text);
+            sc.setRoom(txtRoom.Text);
+            sc.setSchool(txtSchool.Text);
+            Database.Instance.currSchoolclass = sc;
+            DataReader.Instance.createNewSchoolclass(sc);
+        }
+    }
+}
---
+++ ClassM8_Client/ClassM8_Client/Data/Emote.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassM8_Client.Data
+{
+    [DataContract]
+    class Emote
+    {
+        [DataMember]
+        private long id;
+        [DataMember]
+        private String fileName;
+        [DataMember]
+        private String shortString;
+        [DataMember]
+        private long contentSize;
+        [DataMember]
+        private Schoolclass referencedSchoolclass;
+
+        public long getId()
+        {
+            return id;
+        }
+
+        public void setId(long id)
+        {
+            this.id = id;
+        }
+
+        public String getFileName()
+        {
+            return fileName;
+        }
+
+        public void setFileName(String fileName)
+        {
+            this.fileName = fileName;
+        }
+
+        public String getShortString()
+        {
+            return shortString;
+        }
+
+        public void setShortString(String shortString)
+        {
+            this.shortString = shortString;
+        }
+
+        public long getContentSize()
+        {
+            return contentSize;
+        }
+
+        public void setContentSize(long contentSize)
+        {
+            this.contentSize = contentSize;
+        }
+
+        public Schoolclass getReferencedSchoolclass()
+        {
+            return referencedSchoolclass;
+        }
+
+        public void setReferencedSchoolclass(Schoolclass referencedSchoolclass)
+        {
+            this.referencedSchoolclass = referencedSchoolclass;
+        }
+    }
+}
